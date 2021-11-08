79f6ca31a8f1f30789a9738280bb2b2e50d06bc6
Daniel Muller <danielmrezende@yahoo.com.br>
2021-11-08 11:22:35 -0300

    Iniciando operações de processamento do arquivo

--- headers/review.h
+++ headers/review.h
@@ -3,10 +3,15 @@
 
 #include <iostream>
 
+using namespace std;
+
 class Review{
   public:
     Review();
+    Review(string dir);
     ~Review();
+
+    void verificaDir();  // Função para verificar a existência do arquivo binário no diretório.
 
     // Ajeitar funcoes
     void processaReview();
@@ -15,8 +20,15 @@ class Review{
     void testeImportacao();
 
     void iniciar();
+  
   private:
-    
+    string dirArq;       // String para armazenar o diretório do arquivo.
+
+    string review_id;    // String para armazenar o id do comentário.
+    string review_text;  // String para armazenar o comentário.
+    int upvotes;         // String para armazenar os votos favoráveis.
+    string app_version;  // String para armazenar a versão do app;
+    string posted_date;  // String para armazenar a data e hora da postagem.
 };
 
 #endif
