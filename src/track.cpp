44430b0111208dfdc5f592ee21f57c38334394f3
Daniel Muller <danielmrezende@yahoo.com.br>
2021-11-19 11:36:55 -0300

    Adicionando alterações

---
+++ src/track.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <sstream>
+#include <cstring>
+#include <ctime>
+#include "../headers/track.h"
+
+using namespace std;
+
+Track::Track(){
+
+}
+
+Track::~Track(){
+
+}
