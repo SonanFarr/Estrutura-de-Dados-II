5ce6e98d628dc449723e1df48c69fa9937bf1b34
Daniel Muller <danielmrezende@yahoo.com.br>
2021-11-19 11:55:27 -0300

    Colocando alterações

--- headers/review.h
+++ headers/review.h
@@ -2,21 +2,36 @@
 #define REVIEW_H
 
 #include <iostream>
+#include <string.h>
+#include <cstring>
+#include <vector>
+#include "../headers/track.h"
+
+using namespace std;
 
 class Review{
   public:
     Review();
+    Review(string dir);
     ~Review();
 
-    // Ajeitar funcoes
+    void verificaDir();  // Função para verificar a existência do arquivo binário no diretório.
+
+    // Funções principais -----------------------------------------------
     void processaReview();
     void importaRegistros();
-    void acessaRegistro(int index); // imprime na tela
-    void testeImportacao();
+    void acessaRegistro(fstream &arq, int index); // imprime na tela
+    void testeImportacao(ifstream *arqBin, ofstream *outFile);
+    void iniciar(fstream &arq);
+  
 
-    void iniciar();
+    // Funções Auxiliares -----------------------------------------------
+    void escreveTrack(fstream &arq, Track track);
+    void criaTrack(fstream &arq, char id[], char text[], char votes[], char version[], char date[]);
+    Track returnTrack(fstream &arq, int index);
+
   private:
-    
+    string dirArq;       // String para armazenar o diretório do arquivo.
 };
 
 #endif
